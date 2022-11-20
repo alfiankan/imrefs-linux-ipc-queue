faad9e534db2c171558790e8215af0c85ee923f2
alfiankan <alfiankan19@gmail.com>
2022-11-20 15:03:34 +0000

    c interop return char string

--- logic.c
+++ logic.c
@@ -34,14 +34,10 @@ int interop_msgsnd(const char payload[], int payloadLength, int msqid)
     return msgsnd(msqid, &msgp, sizeof(msgp) - sizeof(long), 0);
 }
 
-void interop_msgrcv(int msqid)
+const char interop_msgrcv(int msqid)
 {
     struct Message msgp;
     msgrcv(msqid, &msgp, sizeof(msgp) - sizeof(long), 1, 0);
 
-    for (size_t i = 0; i < msgp.payloadLength; i++)
-    {
-        printf("%c", msgp.payload[i]);
-    }
-    printf("\n");
+    return msgp.payload;
 }
--- publish.nim
+++ publish.nim
@@ -1,9 +1,10 @@
+import os, strutils
 {.compile: "logic.c".}
 proc interop_msgsnd*(payload: cstring, payloadLength: int,
-    msqid: cint): cint {.importc.}
+    msqid: int): cint {.importc.}
 
 proc publishQueue() =
   let greeting: cstring = "Alfiankan nur fathoni"
-  echo interop_msgsnd(greeting, len(greeting), 1)
+  echo interop_msgsnd(greeting, len(greeting), parseInt(paramStr(1)))
 
 publishQueue()
